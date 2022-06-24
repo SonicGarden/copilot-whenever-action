91bcadbc692112e1f8bc2f1a2216674af2a72879
Morikiyo Inoha <morikiyo@sonicgarden.jp>
2022-06-24 15:04:45 +0900

    fix: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-bundle exec ruby ./exec_elastic_whenever.rb $1 $2 "$GITHUB_WORKSPACE/$3"
+set -eux
+
+bundle exec ruby /action/exec_elastic_whenever.rb $1 $2 "$GITHUB_WORKSPACE/$3"
